00e49aa483b5d25e80f9516cf8db0e002360ace6
Andrew Davis <abdavis7@gmail.com>
2020-12-23 00:33:27 -0700

    made children a vector instead of an option<vec>. Made code simpler and a little bit faster.

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,11 @@ use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::time::{Duration, Instant};
 
-const num_threads:usize = 8;
+const num_threads:usize = 4;
 fn main() {
     let start = Instant::now();
     let mut root = Node::new2d(4);
-    while match &root.children {
-        Some(_v) => true,
-        None => false,
-    } {
+    while match root.state.winner{ None => true, _x => false}{
         let start = Instant::now();
         root.calc_scores(20);
         let time = start.elapsed();
@@ -30,72 +27,65 @@ fn main() {
 #[derive(Debug)]
 struct Node<T> {
     state: T,
-    children: Option<Vec<(T, i32)>>,
+    children: Vec<(T, i32)>,
 }
 
 impl Node<Board2d> {
     // This function consumes self and returns a new child
-    fn get_child(self) -> Self {
-        if let Some(mut children) = self.children {
-            if let Some(child) = children.pop() {
-                Node {
-                    state: child.0,
-                    children: None,
-                }
-            } else {
-                panic!()
+    fn get_child(mut self) -> Self {
+        if let Some(child) = self.children.pop() {
+            Node {
+                state: child.0,
+                children: vec![]
             }
         } else {
             panic!()
         }
     }
     fn calc_scores(&mut self, depth: u8) {
-        if let Some(children) = &mut self.children {
-            // Create a channel and mutex here so that threads can communicate
-            let (tx, rx) = mpsc::channel();
-            let mutex = Arc::new(Mutex::new(children.clone()));
-            for _n in 0..min(num_threads, children.len()) {
-                let tx = tx.clone();
-                let mutex = mutex.clone();
-                thread::spawn(move || loop {
-                    let mut children = mutex.lock().unwrap();
-                    let child = children.pop();
-                    // Drop children here to let other threads lock on mutex
-                    drop(children);
-                    match child {
-                        None => break,
-                        Some(mut child) => {
-                            child.1 = -child.0.negamax(MIN + 1, MAX - 1, depth);
-                            tx.send(child).unwrap();
-                        }
+        
+        // Create a channel and mutex here so that threads can communicate
+        let (tx, rx) = mpsc::channel();
+        let mutex = Arc::new(Mutex::new(self.children.clone()));
+        for _n in 0..min(num_threads, self.children.len()) {
+            let tx = tx.clone();
+            let mutex = mutex.clone();
+            thread::spawn(move || loop {
+                let mut children = mutex.lock().unwrap();
+                let child = children.pop();
+                // Drop children here to let other threads lock on mutex
+                drop(children);
+                match child {
+                    None => break,
+                    Some(mut child) => {
+                        child.1 = -child.0.negamax(MIN + 1, MAX - 1, depth);
+                        tx.send(child).unwrap();
                     }
-                });
-            }
-            drop(tx);
-            let mut new_children = vec![];
-            for message in rx {
-                new_children.push(message);
-            }
-            *children = new_children;
-            children.sort_unstable_by_key(|a| a.1);
+                }
+            });
         }
+        drop(tx);
+        let mut new_children = vec![];
+        for message in rx {
+            new_children.push(message);
+        }
+        self.children = new_children;
+        self.children.sort_unstable_by_key(|a| a.1);
     }
 
     fn print_scores(&self) {
-        if let Some(children) = &self.children {
-            let mut out = String::new();
-            for child in children {
-                out.push_str(&child.1.to_string());
-                out.push_str(", ");
-            }
-            println!("{}", out);
+        let mut out = String::new();
+        for child in &self.children {
+            out.push_str(&child.1.to_string());
+            out.push_str(", ");
         }
+        println!("{}", out);
     }
 
     fn new2d(size: usize) -> Node<Board2d> {
         let mut result = Node {
             state: Board2d::new(size),
-            children: None,
+            children: vec![],
         };
         result.make_children();
         result
@@ -104,24 +94,22 @@ impl Node<Board2d> {
     fn make_children(&mut self) {
         // But only if the board is not in an ending state
         if let None = self.state.winner {
-            if let None = self.children {
-                let mut children = vec![];
-                for y in 0..self.state.size {
-                    for x in 0..self.state.size {
-                        if let Space::Blank = self.state.board[x][y] {
-                            children.push((self.state.new_child((x, y)), 0));
-                        }
+            let mut children = vec![];
+            for y in 0..self.state.size {
+                for x in 0..self.state.size {
+                    if let Space::Blank = self.state.board[x][y] {
+                        children.push((self.state.new_child((x, y)), 0));
                     }
                 }
-                // Sets a draw if there are no children
-                if children.is_empty() {
-                    self.state.winner = Some(Space::Blank)
-                }
-                // Otherwise, put the vector in self.children
-                else {
-                    self.children = Some(children)
-                };
             }
+            // Sets a draw if there are no children
+            if children.is_empty() {
+                self.state.winner = Some(Space::Blank)
+            }
+            // Otherwise, put the vector in self.children
+            else {
+                self.children = children
+            };
         }
     }
 }
@@ -129,7 +117,7 @@ impl Node<Board3d> {
     fn new3d(size: usize) -> Node<Board3d> {
         Node {
             state: Board3d::new(size),
-            children: None,
+            children: vec![],
         }
     }
 }
@@ -295,7 +283,7 @@ struct Board3d {
     board: [[[Space; 4]; 4]; 4],
     last: Option<(usize, usize, usize)>,
     size: usize,
-    winner: Option<Space>,
+    winner: WinState
 }
 impl Board3d {
     fn new(size: usize) -> Board3d {
@@ -303,7 +291,7 @@ impl Board3d {
             board: [[[Space::Blank; 4]; 4]; 4],
             last: None,
             size,
-            winner: None,
+            winner: WinState::None,
         }
     }
 }
@@ -313,4 +301,11 @@ enum Space {
     Blank,
     X,
     O,
+}
+#[derive(Copy, Clone, Debug, PartialEq)]
+enum WinState{
+    None,
+    X,
+    O,
+    Draw
 }
